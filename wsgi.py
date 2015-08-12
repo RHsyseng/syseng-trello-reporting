ac46e2da91fc15ac6834e2110aa3efe6411dca9d
Christoph Görn <goern@redhat.com>
2015-08-12 14:54:48 +0200

    added a CSS style

--- wsgi.py
+++ wsgi.py
@@ -44,6 +44,35 @@ def application(environ, start_response):
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <style>
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700");
+html {
+  font-family: sans-serif;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+body {
+  font-family: "Source Sans Pro", Calibri, Candara, Arial, sans-serif;
+  font-size: 15px;
+  line-height: 1.42857143;
+  color: #333333;
+  background-color: #ffffff;
+}
+
+.theme-dropdown .dropdown-menu {
+  position: static;
+  display: block;
+  margin-bottom: 20px;
+}
+
+.theme-showcase > p > .btn {
+  margin: 5px 0;
+}
+
+.theme-showcase .navbar .container {
+  width: auto;
+}
+        </style>
     </head>
     <body>'''
 
